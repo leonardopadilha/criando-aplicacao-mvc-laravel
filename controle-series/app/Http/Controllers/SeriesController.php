4ccbba18bca330a056c1f6673fa2529d1c75f4b0
leonardopadilha <leonardopadilha28@gmail.com>
2023-04-22 07:46:08 -0300

    Session e Flash Messages

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -8,10 +8,12 @@ use App\Models\Serie;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         //$series = Serie::all();
         $series = Serie::query()->orderBy('nome')->get();
+        //$mensagemSucesso = $request->session()->get('mensagem.sucesso');
+        $mensagemSucesso = session('mensagem.sucesso');
 
         //$series = DB::select('SELECT nome FROM series;');
         /* $series = [
@@ -20,7 +22,7 @@ class SeriesController extends Controller
             'Grey\'s Anatomy'
         ]; */
 
-        return view('series.index')->with('series', $series);
+        return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
 
         //return view('listar-series', compact('series'));
 
@@ -43,6 +45,8 @@ class SeriesController extends Controller
         $serie->save(); */
 
         Serie::create($request->all());
+        //session(['mensagem.sucesso' => 'Série adicionada com sucesso']);
+        $request->session()->flash('mensagem.sucesso', 'Série adicionada com sucesso');
         
         //DB::insert('INSERT INTO series (nome) values (?)', [$nomeSerie]);
         //return redirect('/series');
@@ -53,6 +57,7 @@ class SeriesController extends Controller
     public function destroy(Request $request)
     {
         Serie::destroy($request->series);
+        $request->session()->flash('mensagem.sucesso', 'Série removida com sucesso');
         return redirect()->route('series.index');
     }
 }
