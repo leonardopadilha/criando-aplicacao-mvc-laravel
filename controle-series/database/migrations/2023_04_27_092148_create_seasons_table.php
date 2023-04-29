73693dd8a268e570abdaae26c755948fd13717cc
leonardopadilha <leonardopadilha28@gmail.com>
2023-04-29 06:22:43 -0300

    Adicionando temporadas

---
+++ controle-series/database/migrations/2023_04_27_092148_create_seasons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeasonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seasons', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedTinyInteger('numero');
+
+            $table->foreignId('series_id')->constrained();
+            //$table->unsignedBigInteger('series_id');
+            //$table->foreign('series_id')->references('id')->on('series');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seasons');
+    }
+}
